0f021d074736635fc7f67287c837187875a49fe5
ThEndGuy <livm04@hotmail.com>
2026-05-08 21:29:35 +0100

    Added moving lines up and down

--- init.lua
+++ init.lua
@@ -224,10 +224,10 @@ do
   --  Use CTRL+<hjkl> to switch between windows
   --
   --  See `:help wincmd` for a list of all window commands
-  vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-  vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-  vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-  vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+  vim.keymap.set('n', '<C-w>h', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+  vim.keymap.set('n', '<C-w>l', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+  vim.keymap.set('n', '<C-w>j', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+  vim.keymap.set('n', '<C-w>k', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
   -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
   -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -995,3 +995,5 @@ end
 -- My custom keybindings:
 
   vim.keymap.set('n', '<leader>rp', ':w<CR>:!python -X utf8 %<CR>', { desc = '[R]un [P]ython' })
+  vim.keymap.set('n', '<C-j>', ':m+1<CR>', { desc = 'Move line DOWN [j]' })
+  vim.keymap.set('n', '<C-k>', ':m-2<CR>', { desc = 'Move line UP [k]' })
